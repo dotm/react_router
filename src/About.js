11c73de52719e856a16681e266548f5ec7db19d2
Yoshua Elmaryono <desde.el.mar96@gmail.com>
2018-05-08 15:50:43 +0700

    add Executive Profile to demonstrate route params

--- src/About.js
+++ src/About.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import { Route, Link } from "react-router-dom";
+
+function Executive_Profile (props){
+    const {match} = props
+    const {executive} = match.params
+    return (
+        <p>Our {executive.toUpperCase()} is awesome! He has [insert corporate speak here]</p>
+    )
+}
 
 export default function About(props){
+    const { match } = props
     return (
-        <p className="text-center">So, about this company... [insert corporate speak here]</p>
+        <div className="text-center">
+            <p>So, about this company... [insert corporate speak here]</p>
+            
+            <Link to={`${match.url}/ceo`}>CEO</Link>&nbsp;&nbsp;&nbsp;
+            <Link to={`${match.url}/cto`}>CTO</Link>&nbsp;&nbsp;&nbsp;
+            <Link to={`${match.url}/cmo`}>CMO</Link>&nbsp;&nbsp;&nbsp;
+            <Link to={`${match.url}/cfo`}>CFO</Link>&nbsp;&nbsp;&nbsp;
+
+            <Route path={`${match.url}/:executive`} component={Executive_Profile} />
+        </div>
     )
 }
